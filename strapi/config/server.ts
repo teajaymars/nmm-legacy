9a9e102dc86182fe8f51053b9f4179eeab1cfadb
Tom Rees <zephod@gmail.com>
2023-10-12 18:37:18 +0100

    New server.ts

--- strapi/config/server.ts
+++ strapi/config/server.ts
@@ -1,10 +1,36 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-});
+export default ({ env }) => {
+  // Development defaults
+  let host = '127.0.0.1';
+  let port = env.int('PORT', 1337);
+
+  // URL is NOT DYNAMIC. Changing this requires a rebuild.
+  // Dockerfile must pass PUBLIC_URL and NODE_ENV=production at build time.
+  let url: string;
+  if (process.env.NODE_ENV !== 'development') {
+    host = env('HOST', '0.0.0.0');
+    const publicUrl = env('PUBLIC_URL', '');
+    if (!publicUrl) {
+      throw new Error(
+        'Strapi: PUBLIC_URL must be set outside of development mode.'
+      );
+    }
+    url = publicUrl + '/backend';
+  }
+
+  // console.log(`Strapi: NODE_ENV=${process.env.NODE_ENV}. Binding ${host}:${port}`);
+  // if (url) {
+  //   console.log(`Strapi: Admin URL=${url}`);
+  // }
+
+  return {
+    host,
+    port,
+    url,
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+    webhooks: {
+      populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    },
+  };
+};
